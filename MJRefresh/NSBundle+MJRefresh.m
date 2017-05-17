cf1287cdd253c5c527b934e7e32fd8cf2fba4616
徐建波 <492287612@qq.com>
2017-05-17 10:11:45 +0800

    English only

--- MJRefresh/NSBundle+MJRefresh.m
+++ MJRefresh/NSBundle+MJRefresh.m
@@ -38,19 +38,20 @@
 {
     static NSBundle *bundle = nil;
     if (bundle == nil) {
-        // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
-        NSString *language = [NSLocale preferredLanguages].firstObject;
-        if ([language hasPrefix:@"en"]) {
-            language = @"en";
-        } else if ([language hasPrefix:@"zh"]) {
-            if ([language rangeOfString:@"Hans"].location != NSNotFound) {
-                language = @"zh-Hans"; // 简体中文
-            } else { // zh-Hant\zh-HK\zh-TW
-                language = @"zh-Hant"; // 繁體中文
-            }
-        } else {
-            language = @"en";
-        }
+//        // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
+//        NSString *language = [NSLocale preferredLanguages].firstObject;
+//        if ([language hasPrefix:@"en"]) {
+//            language = @"en";
+//        } else if ([language hasPrefix:@"zh"]) {
+//            if ([language rangeOfString:@"Hans"].location != NSNotFound) {
+//                language = @"zh-Hans"; // 简体中文
+//            } else { // zh-Hant\zh-HK\zh-TW
+//                language = @"zh-Hant"; // 繁體中文
+//            }
+//        } else {
+//            language = @"en";
+//        }
+        NSString *language = @"en";//只适配英文
         
         // 从MJRefresh.bundle中查找资源
         bundle = [NSBundle bundleWithPath:[[NSBundle mj_refreshBundle] pathForResource:language ofType:@"lproj"]];
